ad3b449e2c496dd9236c017fb5480e3d05ee0ec7
Cristian Mendivil <CMendivil7@icloud.com>
2019-08-13 16:56:35 -0600

    Getting all files

--- SearchApp/Angular/Services/AllData.js
+++
@@ -1,9 +0,0 @@
-﻿app.factory('alldata', ['$http', function ($http) {
-    return $http.get('/api/getdata')
-        .success(function (data) {
-            return data;
-        })
-        .error(function (err) {
-            return err;
-        });
-}]); 
